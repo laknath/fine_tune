9cdae1c393b428f5d718a4c9dcbe619636e8db96
Laknath <blaknath@gmail.com>
2016-06-19 15:43:08 -0400

    Test setup

--- test/fine_tune_test.rb
+++ test/fine_tune_test.rb
@@ -1,11 +1,46 @@
 require 'test_helper'
 
-class FineTuneTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe FineTune::Base do
+  before do
+  end
+
+  it "returns a version number" do
     refute_nil ::FineTune::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  describe "setting up default limits" do
+    it "returns a singleton" do
+      assert FineTune::Base.instance.equal?(FineTune::Base.instance)
+    end
+
+    it "accepts default options" do
+      FineTune::Base.set_limit(:hourly_emails, window: 1, threshold: 10)
+    end
+
+    it "can override limits" do
+    end
+
+    it "can unset defined rules" do
+    end
+  end
+
+  describe "restricting events for a resource" do
+    it "accepts limit threshold" do
+    end
+
+    it "accepts window option" do
+    end
+
+    it "accepts strategy option" do
+    end
+
+    it "accepts limit threshold" do
+    end
+
+    it "accepts window option" do
+    end
+
+    it "accepts strategy option" do
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fine_tune'
 
+require "minitest/reporters"
 require 'minitest/autorun'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
