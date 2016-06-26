7ff5c837d1a35fa13d8f5639de1e968efb2cca60
Laknath <blaknath@gmail.com>
2016-06-25 20:35:51 -0400

    loading strategies

--- lib/fine_tune.rb
+++ lib/fine_tune.rb
@@ -1,5 +1,17 @@
 require "fine_tune/version"
+require "fine_tune/base"
+
+Dir[File.dirname(__FILE__) + "/fine_tune/strategies/*.rb"].each do |filename|
+  strategy = File.basename(filename, ".rb") 
+  require filename unless strategy == 'base'
+end
+require "fine_tune/strategies/base"
 
 module FineTune
-  # Your code goes here...
+  class << self
+    extend Forwardable
+    def_delegators :"FineTune::Base.instance", :adapter, :adapter=, :count,
+                    :reset, :throttle, :throttle!, :add_limit, :remove_limit, :rate_exceeded?
+  end
 end
+
